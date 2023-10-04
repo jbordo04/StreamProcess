7c1a1c304b58d7cad5690271ed852a12797d4c11
thejbordo <joan.bordonaba@gmail.com>
2023-10-04 11:48:39 +0200

    Metodo readLine, quitado limite de chunks

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 // const file = require('./file.csv')
 import { readFile, createReadStream} from 'node:fs'
+import readline from 'node:readline'
 
 const old = () => {
   console.time()
-  readFile('file-small.csv', 'utf-8', (err, data) => {
-    console.log(data)
+  readFile('file.csv', 'utf-8', (err, data) => {
+    // console.log(data)
     console.timeEnd()
   })
 }
 
+// default: 1.691s
+const newLine = () => {
+  console.time()
+  const readable =  createReadStream('file.csv')
+  
+  const rl = readline.createInterface({
+    input: readable,
+    // Note: we use the crlfDelay option to recognize all instances of CR LF
+    // ('\r\n') in input.txt as a single line break.
+    crlfDelay: Infinity,
+  })
+
+  rl.on('line', (line) => {
+    // console.log(`Line from file: ${line}`);
+  })
+
+  rl.on('close', () => {
+    console.log('finished!')
+    console.timeEnd()
+  })
+}
+
+// default: 3.872s
 const newV = () => {
   console.time()
-  const readable =  createReadStream('file.csv', { highWaterMark: 40 })
+  const readable =  createReadStream('file.csv')
   
   let newLineI = '' 
   let newLine = ''
@@ -19,26 +43,26 @@ const newV = () => {
   readable.on('data', chunk => {
     // console.log({data: chunk.toString()})
 
-    const chunky =  chunk.toString().split('')
+    const chunky =  chunk.toString()
     // console.log(chunky)
 
     //solucion para un salto de linea por chunk \n    
-    const index = chunky.findIndex( letter => letter == '\n')
+    // const index = chunky.findIndex( letter => letter == '\n')
     // console.log('index:',index)
     
-    if(index == -1){
-      const chunkon = chunk.toString()
-      newLineI += chunkon
-      // console.log('-1:',chunkon)
-    } else {
-      newLineI += (chunky.slice(0,index).join(''))
+    // if(index == -1){
+    //   const chunkon = chunk.toString()
+    //   newLineI += chunkon
+    //   // console.log('-1:',chunkon)
+    // } else {
+    //   newLineI += (chunky.slice(0,index).join(''))
 
-      // console.log('liniea:', newLine)
-      newLineI = ''
-      newLineI += (chunky.slice(index).join(''))
-    }    
+    //   // console.log('liniea:', newLine)
+    //   newLineI = ''
+    //   newLineI += (chunky.slice(index).join(''))
+    // }    
 
-    
+
     //solucion dinámica para cualquier longitud.
     
     // let indexSlice = 0
@@ -46,12 +70,12 @@ const newV = () => {
       if(chunky[i] === '\n'){
         // newLine += chunky.slice(indexSlice, i)
         newLine += chunky[i]
-        console.log('linea entera:', newLine)
+        console.log(newLine)
         newLine = ''
-        continue
         // indexSlice = i
+      } else {
+        newLine += chunky[i]
       }
-      newLine += chunky[i]
     }
   })
 
@@ -62,5 +86,5 @@ const newV = () => {
 }
 
 // old()
-
 newV()
+// newLine()
